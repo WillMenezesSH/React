adb3b4e81f83fc6972be8b9e34ea6584bd541b15
WillMenezesSH <wma_1999@hotmail.comcd Portugol/ clearcleargit config --global user.email wma_1999@hotmail.com>
2022-03-21 17:26:20 -0300

    aula 1 react

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+    <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/08/1667.jpg" alt="Imagem Tela Inicial" className='img'/>
+
+    </>
+    );
+}
+
+export default Home;
