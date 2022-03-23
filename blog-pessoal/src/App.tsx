af45abd9b4419d8ac45e4492067b3875bebf4baf
WillMenezesSH <wma_1999@hotmail.comcd Portugol/ clearcleargit config --global user.email wma_1999@hotmail.com>
2022-03-23 10:37:41 -0300

    menu, rodapé e home

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import {Grid} from '@material-ui/core';
 import './App.css';
 import Home from './paginas/home/Home';
 function App() {
   return (
-   <Home/>
+   <>
+   <Navbar />
+   <Home />
+   <Footer/>
+      
+   </>
   );
 }
 
