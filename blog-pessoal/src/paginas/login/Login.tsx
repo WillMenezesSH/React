9ea52534126ae0c7be5ae0eb3bab00e0060e84f3
WillMenezesSH <wma_1999@hotmail.comcd Portugol/ clearcleargit config --global user.email wma_1999@hotmail.com>
2022-03-27 14:20:23 -0300

    Sistemas de rotas implementado

--- blog-pessoal/src/paginas/login/Login.tsx
+++ blog-pessoal/src/paginas/login/Login.tsx
@@ -10,7 +10,7 @@ function Login (){
                 <Box paddingX={20}>
                     <form>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3'
-                         align='center' style={{fontWeight: 'bold'}}>
+                         align='center' className='textos'>
                             Entrar
                             </Typography>
                             <TextField id='usuario' label='Usuário' variant='outlined' name='Usuário' margin='normal' fullWidth ></TextField>
@@ -28,14 +28,11 @@ function Login (){
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                        <Typography variant='subtitle1' gutterBottom align='center' className='textos'>Cadastre-se</Typography>
                     </Box>
                 </Box>
             </Grid>
-            <Grid xs={6} style={{
-                backgroundImage: `url(https://i.imgur.com/wLR9M2w.png)`,
-                backgroundRepeat: 'no-repeat', width: '100vh', minHeight:'100vh', backgroundSize: 'cover', backgroundPosition: 'center',
-                backgroundColor: '#cbc6c6' }}>
+            <Grid xs={6} className='imagem'>
             </Grid>
         </Grid>
 
